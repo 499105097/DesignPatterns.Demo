9147e93176bcd57963a8b3f36f9a3942f03f274f
zhangxin <499105097@qq.com>
2023-06-28 17:50:43 +0800

    feat：升级.NET 7

---
+++ PrototypePattern.Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrototypePattern.Demo
+{
+    /// <summary>
+    /// 原型模式
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
